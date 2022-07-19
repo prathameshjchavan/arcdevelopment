9b70ca26e407f6249095aeffe39ed408a2f55da6
Prathamesh Chavan <prathamesh.chavan216@gmail.com>
2022-07-19 17:18:02 +0530

    hero structure

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,6 +1,10 @@
+// Module Imports
+import { Grid, Button } from "@mui/material";
 import React from "react";
 import Lottie from "react-lottie";
+import ButtonArrow from "../components/ui/ButtonArrow";
 
+// Animations Data
 import animationData from "../animations/landinganimation/data";
 
 function LandingPage() {
@@ -13,7 +17,33 @@ function LandingPage() {
 		},
 	};
 
-	return <Lottie options={defaultOptions} height="100%" width="100%" />;
+	return (
+		<Grid container direction="column">
+			<Grid item>
+				<Grid container direction="row">
+					<Grid item>
+						<div>
+							Bringing West Coast Technology
+							<br />
+							to the Midwest
+						</div>
+						<Grid container>
+							<Grid item>
+								<Button variant="contained">Free Estimate</Button>
+							</Grid>
+							<Grid item>
+								<Button variant="outlined">Learn More</Button>
+								<ButtonArrow width={15} height={15} fill="red" />
+							</Grid>
+						</Grid>
+					</Grid>
+					<Grid item>
+						<Lottie options={defaultOptions} height="100%" width="100%" />
+					</Grid>
+				</Grid>
+			</Grid>
+		</Grid>
+	);
 }
 
 export default LandingPage;
